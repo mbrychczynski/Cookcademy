f50d80b8ca06e52036448efdc83c55615d62881e
Mateusz Brychczynski <mat.brych@gmail.com>
2023-07-25 10:43:39 +0100

    Init commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Mateusz Brychczynski on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
