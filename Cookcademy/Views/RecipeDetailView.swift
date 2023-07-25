1781ef9ffa73055ce05c74dd7a375477f69a26a7
Mateusz Brychczynski <mat.brych@gmail.com>
2023-07-25 16:12:49 +0100

    Adding Color to App

--- Cookcademy/Views/RecipeDetailView.swift
+++ Cookcademy/Views/RecipeDetailView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct RecipeDetailView: View {
     let recipe: Recipe
     
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
     var body: some View {
         VStack {
             HStack {
@@ -19,7 +22,7 @@ struct RecipeDetailView: View {
                 Spacer()
             }
             HStack {
-                Text(recipe.mainInformation.description)
+                Text("Author: \(recipe.mainInformation.description)")
                     .font(.subheadline)
                     .padding()
                 Spacer()
@@ -27,19 +30,21 @@ struct RecipeDetailView: View {
             List {
                 Section(header: Text("Ingredients")) {
                     ForEach(recipe.ingredients.indices, id: \.self) { index in
-                        Text(recipe.ingredients[index].description)
+                        let ingredient = recipe.ingredients[index]
+                        Text(ingredient.description)
+                            .foregroundColor(listTextColor)
                     }
-                }
+                }.listRowBackground(listBackgroundColor)
                 Section(header: Text("Directions")) {
                     ForEach(recipe.directions.indices, id: \.self) { index in
                         let direction = recipe.directions[index]
                         HStack {
-                            Text("\(index + 1).")
-                                .bold()
-                            Text("\(direction.isOptional ? "(Optional) \(direction.description)" : "\(direction.description)")")
-                        }
+                            Text("\(index + 1). ").bold()
+                            Text("\(direction.isOptional ? "(Optional) " : "")"
+                                 + "\(direction.description)")
+                        }.foregroundColor(listTextColor)
                     }
-                }
+                }.listRowBackground(listBackgroundColor)
             }
         }
         .navigationTitle(recipe.mainInformation.name)
--- Cookcademy/Views/RecipesListView.swift
+++ Cookcademy/Views/RecipesListView.swift
@@ -10,11 +10,17 @@ import SwiftUI
 struct RecipesListView: View {
     @StateObject var recipeData = RecipeData()
     
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
     var body: some View {
         List {
             ForEach(recipes) { recipe in
-                NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
+                NavigationLink(recipe.mainInformation.name,
+                               destination: RecipeDetailView(recipe: recipe))
             }
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
         }
         .navigationTitle(navigationTitle)
     }
@@ -29,11 +35,11 @@ struct RecipesListView_Previews: PreviewProvider {
 }
 
 extension RecipesListView {
-  var recipes: [Recipe] {
-    recipeData.recipes
-  }
- 
-  var navigationTitle: String {
-    "All Recipes"
-  }
+    var recipes: [Recipe] {
+        recipeData.recipes
+    }
+    
+    var navigationTitle: String {
+        "All Recipes"
+    }
 }
