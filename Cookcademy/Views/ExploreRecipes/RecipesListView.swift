01e3d960afe42bd48ed06e1e46f61890cd8ceac0
Mateusz Brychczynski <mat.brych@gmail.com>
2023-07-31 17:15:39 +0100

    Preparing the Recipes for Modification

--- Cookcademy/Views/ExploreRecipes/RecipesListView.swift
+++ Cookcademy/Views/ExploreRecipes/RecipesListView.swift
@@ -21,7 +21,7 @@ struct RecipesListView: View {
         List {
             ForEach(recipes) { recipe in
                 NavigationLink(recipe.mainInformation.name,
-                               destination: RecipeDetailView(recipe: recipe))
+                               destination: RecipeDetailView(recipe: binding(for: recipe)))
             }
             .listRowBackground(listBackgroundColor)
             .foregroundColor(listTextColor)
@@ -76,5 +76,12 @@ extension RecipesListView {
     
     private var navigationTitle: String {
         "\(category.rawValue) Recipes"
+    }
+    
+    func binding(for recipe: Recipe) -> Binding<Recipe> {
+        guard let index = recipeData.index(of: recipe) else {
+            fatalError("Recipe not found")
+        }
+        return $recipeData.recipes[index]
     }
 }
