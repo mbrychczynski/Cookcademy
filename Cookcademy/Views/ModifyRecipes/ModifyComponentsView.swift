fa5a3fd52437129f1cb8b394350991dacf239204
Mateusz Brychczynski <mat.brych@gmail.com>
2023-08-01 15:24:29 +0100

    Modifying Individual Components/ Reordering and Deleting Components

--- Cookcademy/Views/ModifyRecipes/ModifyComponentsView.swift
+++ Cookcademy/Views/ModifyRecipes/ModifyComponentsView.swift
@@ -55,14 +55,19 @@ struct ModifyComponentsView<Component: RecipeComponent, DestinationView: ModifyC
                 List {
                     ForEach(components.indices, id: \.self) { index in
                         let component = components[index]
-                        Text(component.description)
+                        let editComponentView = DestinationView(component: $components[index]) { _ in return }
+                            .navigationTitle("Edit \(Component.singularName().capitalized)")
+                        NavigationLink(component.description, destination: editComponentView)
                     }
+                    .onDelete { components.remove(atOffsets: $0) }
+                    .onMove { indices, newOffet in components.move(fromOffsets: indices, toOffset: newOffet) }
                     .listRowBackground(listBackgroundColor)
                     NavigationLink("Add another \(Component.singularName())",
                                    destination: addComponentView)
                     .buttonStyle(PlainButtonStyle())
                     .listRowBackground(listBackgroundColor)
-                }.foregroundColor(listTextColor)
+                }
+                .foregroundColor(listTextColor)
             }
         }
     }
