892b19c354e2c21d1b4707364b40a899143e73df
Mateusz Brychczynski <mat.brych@gmail.com>
2023-08-01 16:00:15 +0100

    Displaying the Settings Page

--- Cookcademy/Views/MainTabView.swift
+++ Cookcademy/Views/MainTabView.swift
@@ -8,18 +8,20 @@
 import SwiftUI
 
 struct MainTabView: View {
-    @StateObject var recipeData = RecipeData()
-    
-    var body: some View {
-        TabView {
-            RecipeCategoryGridView()
-                .tabItem { Label("Recipes", systemImage: "list.dash") }
-            NavigationView {
-                RecipesListView(viewStyle: .favorites)
-            }.tabItem { Label("Favorites", systemImage: "heart.fill") }
-        }
-        .environmentObject(recipeData)
+  @StateObject private var recipeData = RecipeData()
+ 
+  var body: some View {
+    TabView {
+      RecipeCategoryGridView()
+        .tabItem { Label("Recipes", systemImage: "list.dash") }
+      NavigationView {
+        RecipesListView(viewStyle: .favorites)
+      }.tabItem { Label("Favorites", systemImage: "heart.fill") }
+      SettingsView()
+        .tabItem { Label("Settings", systemImage: "gear") }
     }
+    .environmentObject(recipeData)
+  }
 }
 
 struct MainTabView_Previews: PreviewProvider {
